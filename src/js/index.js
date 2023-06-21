a290aa273404d1dda97c3c76d3148c858546efff
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-06-20 19:42:44 -0600

    Agregue el chat de Homero

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,38 @@
-const btnDona = document.getElementById("donutImage");
+const imgDonut = document.getElementById("donutImage");
 const clickSound = document.getElementById("clickSound");
-btnDona.addEventListener("click", function () {
+imgDonut.addEventListener("click", function () {
   clickSound.play();
-  btnDona.classList.remove("grayscale");
-  btnDona.classList.remove("w-52");
-  btnDona.classList.remove("h-52");
-  btnDona.classList.add("w-48");
-  btnDona.classList.add("h-48");
+  imgDonut.classList.remove("grayscale");
+  imgDonut.classList.remove("w-52");
+  imgDonut.classList.remove("h-52");
+  imgDonut.classList.add("w-48");
+  imgDonut.classList.add("h-48");
   setTimeout(function () {
-    btnDona.src = "./src/media/dona-mordida.png";
+    imgDonut.src = "./src/media/dona-mordida.png";
+    typeMessage(
+      "!Oye, te dije que podías tomar un bocado, pero no la mitad de la rosquilla!",
+      4
+    );
   }, 600);
 });
+
+function typeMessage(message, time = 7) {
+  const chatMessage = document.getElementById("chat-message");
+  const messages = ["", ".", "..", "..."];
+  let i = 0;
+
+  const typing = setInterval(() => {
+    if (i === time) {
+      clearInterval(typing);
+      chatMessage.textContent = message;
+      return;
+    }
+
+    const currentMessage = messages[i % messages.length];
+    chatMessage.textContent = currentMessage;
+
+    i++;
+  }, 400);
+}
+
+typeMessage("D'oh! ¡Dale un bocado a la rosquilla!", 7);
