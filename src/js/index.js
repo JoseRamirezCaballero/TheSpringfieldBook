443820487595ac0828396dcd6b8fa012c12ec376
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-06-20 20:26:45 -0600

    Agregue musica de fondo

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,50 @@
+const bgMusic = document.getElementById("bg-music");
+const playButton = document.getElementById("play-button");
+const playIcon = document.getElementById("play-icon");
+
+playButton.addEventListener("click", function () {
+  if (bgMusic.paused) {
+    bgMusic.play();
+    typeMessage("¡Woo-hoo! ¡La música es lo máximo!", 4);
+    playIcon.innerHTML = `
+    <svg
+      aria-hidden="true"
+      class="w-8 h-8"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M6 4h4v16H6zM14 4h4v16h-4z"
+      ></path>
+    </svg>  
+    `;
+  } else {
+    bgMusic.pause();
+    playIcon.innerHTML = `
+      <svg
+        aria-hidden="true"
+        class="w-8 h-8"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M7 4v16l13-8z"
+        ></path>
+      </svg>
+    `;
+  }
+});
+
 const imgDonut = document.getElementById("donutImage");
 const clickSound = document.getElementById("clickSound");
 imgDonut.addEventListener("click", function () {
@@ -18,21 +65,20 @@ imgDonut.addEventListener("click", function () {
 
 function typeMessage(message, time = 7) {
   const chatMessage = document.getElementById("chat-message");
-  const messages = ["", ".", "..", "..."];
+  const dots = ["", ".", "..", "..."];
   let i = 0;
 
-  const typing = setInterval(() => {
+  const typingEffect = setInterval(() => {
     if (i === time) {
-      clearInterval(typing);
+      clearInterval(typingEffect);
       chatMessage.textContent = message;
       return;
     }
 
-    const currentMessage = messages[i % messages.length];
-    chatMessage.textContent = currentMessage;
+    const typing = dots[i % dots.length];
+    chatMessage.textContent = typing;
 
     i++;
   }, 400);
 }
-
 typeMessage("D'oh! ¡Dale un bocado a la rosquilla!", 7);
