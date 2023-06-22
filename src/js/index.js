ff29f28614ba842e6332af1ae57359e539a8736c
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-06-21 18:14:29 -0600

    Agregue funcionalidad de Modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,10 @@
-const bgMusic = document.getElementById("bg-music");
 const playButton = document.getElementById("play-button");
-const playIcon = document.getElementById("play-icon");
-
 playButton.addEventListener("click", function () {
+  const bgMusic = document.getElementById("bg-music");
+  const playIcon = document.getElementById("play-icon");
   if (bgMusic.paused) {
     bgMusic.play();
-    typeMessage("¡Woo-hoo! ¡La música es lo máximo!", 4);
+    typeMessage(obtenerFraseHomero("Musica"), 4);
     playIcon.innerHTML = `
     <svg
       aria-hidden="true"
@@ -56,10 +55,7 @@ imgDonut.addEventListener("click", function () {
   imgDonut.classList.add("h-48");
   setTimeout(function () {
     imgDonut.src = "./src/media/dona-mordida.png";
-    typeMessage(
-      "!Oye, te dije que podías tomar un bocado, pero no la mitad de la rosquilla!",
-      4
-    );
+    typeMessage(obtenerFraseHomero("Rosquilla"), 4);
   }, 600);
 });
 
@@ -82,3 +78,167 @@ function typeMessage(message, time = 7) {
   }, 400);
 }
 typeMessage("D'oh! ¡Dale un bocado a la rosquilla!", 7);
+
+const characters = async () => {
+  try {
+    const response = await fetch(
+      "https://apisimpsons.fly.dev/api/personajes?limit=650"
+    );
+    const data = await response.json();
+    return data.docs.filter(
+      (character) =>
+        character.Nombre !== "Spud & Cooder" &&
+        character.Nombre !== "Jebediah Springfield"
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+let vivo,
+  fallecido,
+  ficticio,
+  robot,
+  desconocido,
+  divino,
+  otro,
+  musica,
+  rosquilla;
+
+fetch("/api/chatHomero.json")
+  .then((response) => response.json())
+  .then((data) => {
+    vivo = data.Vivo;
+    fallecido = data.Fallecido;
+    ficticio = data.Ficticio;
+    robot = data.Robot;
+    desconocido = data.Desconocido;
+    divino = data.Divino;
+    otro = data.Otro;
+    musica = data.Musica;
+    rosquilla = data.Rosquilla;
+  })
+  .catch((error) => {
+    console.error("Error al cargar el archivo JSON:", error);
+  });
+
+const imageContainer = document.getElementById("image-container");
+
+(async () => {
+  try {
+    const charactersData = await characters();
+    charactersData.forEach((character) => {
+      const divElement = document.createElement("div");
+      divElement.className =
+        "flex items-center justify-center mb-2 bg-[#0097ce] rounded-xl cursor-pointer";
+
+      const imgElement = document.createElement("img");
+      imgElement.className = "h-48 max-w-full rounded-lg p-2";
+      imgElement.src = character.Imagen;
+      imgElement.alt = character.Nombre;
+
+      divElement.appendChild(imgElement);
+
+      divElement.addEventListener("click", () => {
+        const sectionModal = document.querySelector("#modal");
+        const nombreElement = sectionModal.querySelector("#nombre");
+        const estadoElement = sectionModal.querySelector("#estado");
+        const historiaElement = sectionModal.querySelector("#historia");
+        const contenedorEstado = document.getElementById("contenedorEstado");
+
+        const infoAdicionalElement =
+          sectionModal.querySelector("#infoAdicional");
+
+        nombreElement.textContent = character.Nombre;
+        historiaElement.textContent = character.Historia;
+        infoAdicionalElement.textContent = `¡Ay, caramba! Este personaje es tan extraordinario que dejará a todos en un estado de asombro absoluto. Resulta que es del género ${character.Genero} y se desempeña en el hilarante oficio de ${character.Ocupacion}. Así que prepárate para pasar momentos inolvidables junto a ${character.Nombre}`;
+
+        let estado = "";
+
+        if (character.Estado === "Vivo") {
+          contenedorEstado.classList.add("bg-green-100", "text-green-800");
+          estado = "La vida de esta alma aún resplandece con la gracia divina";
+        } else if (character.Estado === "Fallecido") {
+          contenedorEstado.classList.add("bg-red-100", "text-red-800");
+          estado =
+            "Sus días en la tierra han terminado, pero su espíritu vive con el Todopoderoso";
+        } else if (character.Estado === "Ficticio") {
+          contenedorEstado.classList.add("bg-purple-100", "text-purple-800");
+          estado =
+            "Aunque ficticio en su existencia, su influencia perdura en los corazones";
+        } else if (character.Estado === "Robot") {
+          contenedorEstado.classList.add("bg-blue-100", "text-blue-800");
+          estado =
+            "Es un robot. ¿Sabías que los robots tienen una vida dura? ¡Siempre tienen muchos 'problemas' para resolver!";
+        } else if (character.Estado === "Desconocido") {
+          contenedorEstado.classList.add("bg-gray-100", "text-gray-800");
+          estado =
+            "Encomendamos al misterio divino el alma de aquel cuyo origen desconocemos";
+        } else if (character.Estado === "Divino") {
+          contenedorEstado.classList.add("bg-yellow-100", "text-yellow-800");
+          estado =
+            "¡Este ser es divino! Su presencia ilumina nuestras vidas con su gracia infinita";
+        } else {
+          contenedorEstado.classList.add("bg-gray-100", "text-gray-800");
+          estado = "Un enigma sin resolver";
+        }
+        typeMessage(obtenerFraseHomero(character.Estado), 5);
+        estadoElement.textContent = estado;
+
+        sectionModal.classList.remove("hidden");
+      });
+
+      imageContainer.appendChild(divElement);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const closeButton = document.querySelector("#close");
+
+closeButton.addEventListener("click", () => {
+  const sectionModal = document.querySelector("#modal");
+  typeMessage(
+    "¡Ay caramba! ¡Vamos a explorar otro personaje, a ver si hay alguien más emocionante que yo en esta historia! ¡D'oh!",
+    4
+  );
+  sectionModal.classList.add("hidden");
+});
+
+function obtenerFraseHomero(estado) {
+  let frase;
+
+  switch (estado) {
+    case "Vivo":
+      frase = vivo;
+      break;
+    case "Fallecido":
+      frase = fallecido;
+      break;
+    case "Ficticio":
+      frase = ficticio;
+      break;
+    case "Robot":
+      frase = robot;
+      break;
+    case "Desconocido":
+      frase = desconocido;
+      break;
+    case "Divino":
+      frase = divino;
+      break;
+    case "Musica":
+      frase = musica;
+      break;
+    case "Rosquilla":
+      frase = rosquilla;
+      break;
+    default:
+      frase = otro;
+      break;
+  }
+
+  const indice = Math.floor(Math.random() * frase.length);
+  return frase[indice];
+}
